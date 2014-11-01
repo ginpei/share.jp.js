9f341f1e31fdf805b8456264c2c94daa4fa8a20a
ginpei <ginpei@gmail.com>
2014-11-01 21:20:57 +0900

    いいね！ボタン

--- share.jp.js
+++ share.jp.js
@@ -1,4 +1,5 @@
 (function() {
+	var elBody = document.body;
 	var gp = window.gp = window.gp || {};
 
 	// API
@@ -24,11 +25,30 @@
 		}
 	}
 
-	// TODO
+	// Facebook共通の初期処理
+	share._initFacebook = function() {
+		if (!share._fbLoaded) {
+			appendHtml('<div id="fb-root"></div>');
+			var elFbScript = document.createElement('SCRIPT');
+			elFbScript.text =
+				'(function(d, s, id) {\n' +
+				'	var js, fjs = d.getElementsByTagName(s)[0];\n' +
+				'	if (d.getElementById(id)) return;\n' +
+				'	js = d.createElement(s); js.id = id;\n' +
+				'	js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&appId=1513442402227370&version=v2.0";\n' +
+				'	fjs.parentNode.insertBefore(js, fjs);\n' +
+				'}(document, "script", "facebook-jssdk"));'
+				;
+			elBody.appendChild(elFbScript);
+			share._fbLoaded = true;
+		}
+	};
+
+	// いいね！ボタン
+	// https://developers.facebook.com/docs/plugins/like-button
 	share.like = function(elBlock) {
-		var elButton = document.createElement('DIV');
-		elButton.appendChild(document.createTextNode('LIKE'));
-		elBlock.appendChild(elButton);
+		share._initFacebook();
+		appendHtml('<div class="fb-like" data-href="https://example.com" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>', elBlock);
 	}
 
 	// TODO
@@ -64,6 +84,19 @@
 		var elButton = document.createElement('DIV');
 		elButton.appendChild(document.createTextNode('MIXI'));
 		elBlock.appendChild(elButton);
+	}
+
+	// Util
+	var elDummy = document.createElement('DIV');
+	function html(source) {
+		elDummy.innerHTML = source;
+		var elResult = elDummy.firstChild;
+		elDummy.innerHTML = '';
+		return elResult
+	}
+	function appendHtml(source, elParent) {
+		var el = html(source);
+		(elParent || elBody).appendChild(el);
 	}
 
 	gp.share();
